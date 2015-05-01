04764b0569eb0edb63e12df579e5cec1147e4e49
Randy Leighton <randyleighton@Leighton-C2QP209CG973.local>
2015-04-30 17:10:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnowledgeABLE::Application.config.secret_token = '0ee9bdc2e56b8514a1bce8022d15671e84ef92a44f13f01d404b278042e0e2f830584531be74e1d964c003da1f86cc48ba67045bed8d3bfd685aec8f9d0b1ace'
